b0153c1ff6d3e801a1dd1fd85b193c8695703aba
nodmentor2ygn <nodmentor2ygn@gmail.com>
2020-12-16 15:01:24 +0630

    creating_YGN_outfits

---
+++ db/schema.rb
@@ -0,0 +1,113 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_02_023023) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "customer_comments", force: :cascade do |t|
+    t.bigint "evaluate_id", null: false
+    t.bigint "customer_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_customer_comments_on_customer_id"
+    t.index ["evaluate_id"], name: "index_customer_comments_on_evaluate_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "display_name", default: "[未設定]"
+    t.string "full_name", null: false
+    t.string "icon"
+    t.integer "score", default: 0
+    t.string "residence", default: "[未設定]"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "evaluates", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "customer_id"
+    t.float "rate"
+    t.string "image"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_evaluates_on_customer_id"
+    t.index ["product_id", "customer_id"], name: "index_evaluates_on_product_id_and_customer_id", unique: true
+    t.index ["product_id"], name: "index_evaluates_on_product_id"
+  end
+
+  create_table "maker_comments", force: :cascade do |t|
+    t.bigint "evaluate_id", null: false
+    t.bigint "maker_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["evaluate_id"], name: "index_maker_comments_on_evaluate_id"
+    t.index ["maker_id"], name: "index_maker_comments_on_maker_id"
+  end
+
+  create_table "makers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
+    t.string "site_url", default: "http://example.com"
+    t.text "overview"
+    t.string "logo"
+    t.string "image"
+    t.string "ivent"
+    t.index ["email"], name: "index_makers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_makers_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "price"
+    t.string "image"
+    t.string "season"
+    t.text "overview"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "maker_id"
+    t.index ["maker_id"], name: "index_products_on_maker_id"
+  end
+
+  add_foreign_key "customer_comments", "customers"
+  add_foreign_key "customer_comments", "evaluates"
+  add_foreign_key "maker_comments", "evaluates"
+  add_foreign_key "maker_comments", "makers"
+  add_foreign_key "products", "makers"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,396 @@
+Maker.create!(
+  [
+    {
+      name: "KKH OS",
+      email: "khantkyawhtet@gmail.com",
+      password: "1234567890",
+      site_url: "https://khant.kkhos.mm",
+      overview: "𝐧𝐞𝐰 𝐬𝐰𝐞𝐚𝐭𝐞𝐫 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧🍂🍁
+                •size - M/L/XL/2XL
+                •preorder/ ",
+      logo: open("#{Rails.root}/db/seed_fixtures/sweater1.jpg"),
+      image: open("#{Rails.root}/db/seed_fixtures/sweater2.jpg"),
+      ivent: "Instock YGN_outfits 𝙲𝚕𝚘𝚝𝚑𝚒𝚗𝚐 𝚌𝚘𝚕𝚕𝚎𝚌𝚝𝚒𝚘𝚗𝚜"
+    },
+    {
+      name: "Hyper Clothing",
+      email: "hyperclothing@gmail.com",
+      password: "1234567890",
+      site_url: "https://hyperclothing.os",
+      overview: "✔️material - Stainless / Titanium Steel",
+      logo: open("#{Rails.root}/db/seed_fixtures/hyperclothing.jpg"),
+      image: open("#{Rails.root}/db/seed_fixtures/handchain1.jpg"),
+      ivent: "[‼️INSTOCK‼️ ] 𝐭𝐫𝐞𝐧𝐝𝐲 𝐡𝐚𝐧𝐝-𝐜𝐡𝐚𝐢𝐧 𝐢𝐧𝐬𝐭𝐨𝐜𝐤 𝐬𝐚𝐥𝐞"
+    },
+    {
+      name: "U&I",
+      email: "u&ios@gmail.com",
+      password: "1234567890",
+      site_url: "https://u&i.os.clothing.ygn",
+      overview: "𝐈𝐍𝐒𝐓𝐎𝐂𝐊 ‼️
+                •size - S/M/L/XL/2XL
+                •preorder/ ",
+      logo: open("#{Rails.root}/db/seed_fixtures/u&i.jpg"),
+      image: open("#{Rails.root}/db/seed_fixtures/jeanspant.jpg"),
+      ivent: "𝐧𝐞𝐰 𝐉𝐞𝐚𝐧 𝐩𝐚𝐧𝐭𝐬 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧"
+    },
+    {
+      name: "Radiance",
+      email: "radiance@gmail.com",
+      password: "1234567890",
+      site_url: "https://radiance.os.com",
+      overview: "𝟏𝟗𝐒𝐭𝐮𝐝𝐢𝐨 / 𝐀𝐮𝐭𝐮𝐦𝐧-𝐖𝐢𝐧𝐭𝐞𝐫 𝐧𝐞𝐰 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧 𝐂𝐨𝐚𝐭
+                  Original price - 41000
+                  Discount price - ONLY 25000
+                  Size - ‼️only LARGE size ‼️",
+      logo: open("#{Rails.root}/db/seed_fixtures/radiance.jpg"),
+      image: open("#{Rails.root}/db/seed_fixtures/coat1.jpg"),
+      ivent: "🚨SALE - INSTOCK COAT"
+    },
+    {
+      name: "Standout Loud",
+      email: "standoutloud@gmail.com",
+      password: "1234567890",
+      site_url: "https://mm.standout/lout.com",
+      overview: "✔️material - leather
+                ✔️unisex
+                ✔️Only limited quantities
+                ✔️ygn/mdy cash on delivery 📦
+                Grab yours before its sold out again !! ",
+      logo: open("#{Rails.root}/db/seed_fixtures/standoutloud.jpg"),
+      image: open("#{Rails.root}/db/seed_fixtures/belt1.jpg"),
+      ivent: "𝐈𝐍𝐒𝐓𝐎𝐂𝐊 / 𝐕𝐢𝐧𝐭𝐚𝐠𝐞 𝐥𝐞𝐚𝐭𝐡𝐞𝐫 𝐛𝐞𝐥𝐭 𝐚𝐫𝐞 𝐛𝐚𝐜𝐤 𝐢𝐧𝐬𝐭𝐨𝐜𝐤 (unisex)"
+    },
+    {
+      name: "My Delight",
+      email: "mydelight@gmail.com",
+      password: "1234567890",
+      site_url: "https://mydelight.os.com/mm",
+      overview: "𝐧𝐞𝐰 𝐬𝐰𝐞𝐚𝐭𝐬𝐡𝐢𝐫𝐭 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧
+                •size - S/M/L/XL
+                •preorder/ ",
+      logo: open("#{Rails.root}/db/seed_fixtures/mydelight.jpg"),
+      image: open("#{Rails.root}/db/seed_fixtures/sweatshirt1.jpg"),
+      ivent: "Instock 𝐧𝐞𝐰 𝐬𝐰𝐞𝐚𝐭𝐬𝐡𝐢𝐫𝐭 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧"
+    },
+    {
+      name: "MK Branded Online Shopping",
+      email: "mkbos@gmail.com",
+      password: "1234567890",
+      site_url: "https://mkbranded.online/shop.com",
+      overview: "[𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐀++] 𝐬𝐧𝐞𝐚𝐤𝐞𝐫 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧𝐬👟💥
+                  •waiting times -2/3weeks
+                  •preorder/ ",
+      logo: open("#{Rails.root}/db/seed_fixtures/mkonlineshopping.jpg"),
+      image: open("#{Rails.root}/db/seed_fixtures/shoe1.jpg"),
+      ivent: "Sale [𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐀++] 𝐬𝐧𝐞𝐚𝐤𝐞𝐫 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧𝐬👟💥"
+    }
+  ]
+)
+
+Product.create!(
+  [
+    {
+      maker_id: 1,
+      name: "Sleeve Shirt",
+      overview: "𝐜𝐚𝐬𝐮𝐚𝐥 𝐰𝐞𝐚𝐫 𝐬𝐡𝐨𝐫𝐭 𝐬𝐥𝐞𝐞𝐯𝐞 𝐬𝐡𝐢𝐫𝐭 🍃
+                •size - M/L/XL/2XL
+                •color - 6colors
+                •preorder/ ",
+      price: "1500~2200Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/sleeveshirt1.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 1,
+      name: "Sleeve Casual Shirt",
+      overview: "𝐌𝐢𝐧𝐢𝐦𝐚𝐥𝐢𝐬𝐭 𝐥𝐨𝐧𝐠 𝐬𝐥𝐞𝐞𝐯𝐞 𝐜𝐚𝐬𝐮𝐚𝐥 𝐬𝐡𝐢𝐫𝐭🖤
+                •size - M/L/XL/2XL
+                •preorder/",
+      price: "1800~2500Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/sleevecasualshirt.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 1,
+      name: "Chain Necklaces",
+      overview: "𝐈𝐍𝐒𝐓𝐎𝐂𝐊‼️ 𝐂𝐡𝐚𝐢𝐧 𝐧𝐞𝐜𝐤𝐥𝐚𝐜𝐞𝐬 𝐚𝐫𝐞 𝐛𝐚𝐜𝐤 𝐢𝐧𝐬𝐭𝐨𝐜𝐤 🔥
+                ✔️material - Stainless / Titanium Steel
+                ✔️Only limited quantities
+                Grab yours before its sold out again !!",
+      price: "1400Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/chainnecklaces.jpg"),
+      season: "1week"
+    },
+    {
+      maker_id: 1,
+      name: "Tote Bag",
+      overview: "𝐇𝐘𝐏𝐄’𝐬 𝐓𝐨𝐭𝐞 𝐁𝐚𝐠 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧𝐬  🍃♻️
+                •preorder/ ",
+      price: "9000Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/totebag1.jpg"),
+      # image: open("#{Rails.root}/db/seed_fixtures/zuta.jpg"),
+      #この画像をherokuでdb:seedするとなぜかエラーがでてしまう
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 1,
+      name: "Mogern Studio /𝟐𝟎𝐬𝐬 𝐂𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧",
+      price: "20000~30000Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/mogern.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 2,
+      name: "COLN /𝟐𝟎𝐬𝐬 𝐂𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧 ☁️",
+      price: "1800Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/coln.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 2,
+      name: "19Studio / 𝟐𝟎𝐬𝐬 𝐂𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧",
+      price: "22000Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/19studio.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 3,
+      name: "𝐃𝐚𝐳𝐨 𝐒𝐭𝐮𝐝𝐢𝐨 / 𝟐𝟎𝟐𝟎 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧𝐬",
+      price: "2800Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/dazo.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 3,
+      name: "𝐃𝐚𝐳𝐨 𝐒𝐭𝐮𝐝𝐢𝐨 / 𝐂𝐚𝐬𝐮𝐚𝐥 𝐭𝐞𝐞 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧𝐬",
+      price: "1600Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/casualtee.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 3,
+      name: "Attitude Studio  / 𝐧𝐞𝐰 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧☁️",
+      price: "3000円",
+      image: open("#{Rails.root}/db/seed_fixtures/attitude.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 4,
+      name: "𝟏𝟗 𝐒𝐭𝐮𝐝𝐢𝐨 / 𝐧𝐞𝐰 𝐭𝐞𝐞 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧",
+      overview: "•size - M/L/XL
+                  •preorder",
+      price: "22000Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/newtee.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 5,
+      name: "𝐁𝐞𝐧𝐞 𝐒𝐭𝐮𝐝𝐢𝐨 / 𝐧𝐞𝐰 𝐜𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧",
+      overview: "•size M/L/XL
+                •preorder",
+      price: "1800Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/bene.jpg"),
+      season: "2/3weeks"
+    },
+    {
+      maker_id: 5,
+      name: "𝐌𝐑𝐂𝐘𝐂 𝐬𝐭𝐮𝐝𝐢𝐨 / 𝐬𝐪𝐮𝐚𝐫𝐞 𝐦𝐢𝐧𝐢 𝐛𝐚𝐠",
+      overview: "•waiting times -2/3weeks
+                  •preorder
+                  (instock‼️)",
+      price: "15000Ks",
+      image: open("#{Rails.root}/db/seed_fixtures/squarebag.jpg"),
+      season: "instock"
+    }
+  ]
+)
+
+Customer.create!(
+  [
+    {
+      display_name: "KangMin",
+      full_name: "LeeKangMin",
+      email: "kangmin11@gmail.com",
+      password: "1234567890",
+      icon: open("#{Rails.root}/db/seed_fixtures/kangmin.jpg"),
+      score: 80,
+      residence: "Seoul"
+    },
+    {
+      display_name: "Shunsuke",
+      full_name: "TakahashiShunsuke",
+      password: "1234567890",
+      email: "shunsuke99@gmail.com",
+      icon: open("#{Rails.root}/db/seed_fixtures/shunsuke.jpg"),
+      score: 40,
+      residence: "Tokyo"
+    },
+    {
+      display_name: "Yuuta",
+      full_name: "SatouYuuta",
+      password: "1234567890",
+      email: "yuutast@gmail.com",
+      icon: open("#{Rails.root}/db/seed_fixtures/yuuta.jpg"),
+      score: 100,
+      residence: "Osaka"
+    },
+    {
+      display_name: "Minjae",
+      full_name: "KimMinJae",
+      password: "1234567890",
+      email: "minjaekim@gmail.com",
+      icon: open("#{Rails.root}/db/seed_fixtures/minjae.jpg"),
+      score: 20,
+      residence: "Bunsan"
+    },
+    {
+      display_name: "Yuno",
+      full_name: "MichiedaYuno",
+      password: "1234567890",
+      email: "yuno12@gmail.com",
+      icon: open("#{Rails.root}/db/seed_fixtures/yuno.jpg"),
+      score: 0,
+      residence: "Tokyo/Shinjuku"
+    }
+  ]
+)
+
+Evaluate.create!(
+  [
+    {
+      product_id: 1,
+      customer_id: 2,
+      rate: 3.5,
+      image: open("#{Rails.root}/db/seed_fixtures/sleeveshirt1.jpg"),
+      content: "I like it, but there is no S size."
+    },
+    {
+      product_id: 6,
+      customer_id: 3,
+      rate: 4.5,
+      image: open("#{Rails.root}/db/seed_fixtures/handchain1.jpg"),
+      content: "Since it was used in Stainless / Titanium Steel, it can be used for a long time. It's recommended！"
+    },
+    {
+      product_id: 11,
+      customer_id: 1,
+      rate: 5.0,
+      image: open("#{Rails.root}/db/seed_fixtures/attitude.jpg"),
+      content: "I have this.It's a little expensive, but I really like it. I recommend it."
+    },
+    {
+      product_id: 11,
+      customer_id: 2,
+      rate: 3.0,
+      image: open("#{Rails.root}/db/seed_fixtures/totebag1.jpg"),
+      content: "Do you have any other designs? I'd like the color to be black as well."
+    },
+    {
+      product_id: 13,
+      customer_id: 1,
+      rate: 2.5,
+      image: open("#{Rails.root}/db/seed_fixtures/squarebag.jpg"),
+      content: "I bought it. I thought it was too small, but it's more convenient than I expected."
+    },
+    {
+      product_id: 3,
+      customer_id: 1,
+      rate: 2.0,
+      content: "I wonder if the price is a little high."
+    },
+    {
+      product_id: 3,
+      customer_id: 5,
+      rate: 3.0,
+      content: "I like it as a design, but are there any other colors?"
+    },
+    {
+      product_id: 10,
+      customer_id: 5,
+      rate: 5.0,
+      image: open("#{Rails.root}/db/seed_fixtures/attitude.jpg"),
+      content: "I really like it. I think it looks good when I wear it."
+    }
+  ]
+)
+
+MakerComment.create!(
+  [
+    {
+      evaluate_id: 1,
+      maker_id: 3,
+      content: "Thank you for your feedback."
+    },
+    {
+      evaluate_id: 3,
+      maker_id: 4,
+      content: "Thank you for your support."
+    },
+    {
+      evaluate_id: 5,
+      maker_id: 5,
+      content: "Thank you for your feedback."
+    },
+    {
+      evaluate_id: 8,
+      maker_id: 3,
+      content: "Thank you for your patronage. We are planning to release a new product, so we will inform you later."
+    }
+  ]
+)
+
+CustomerComment.create!(
+  [
+    {
+      evaluate_id: 1,
+      customer_id: 3,
+      content: "There are many colors and the price is just right."
+    },
+    {
+      evaluate_id: 1,
+      customer_id: 1,
+      content: "Beautiful ~ I want to buy it now."
+    },
+    {
+      evaluate_id: 2,
+      customer_id: 2,
+      content: "I already bought it. It's really good. I recommend it."
+    },
+    {
+      evaluate_id: 2,
+      customer_id: 3,
+      content: "I love this design."
+    },
+    {
+      evaluate_id: 3,
+      customer_id: 1,
+      content: "Looks cool. Of course it suits me. LoL"
+    },
+    {
+      evaluate_id: 3,
+      customer_id: 4,
+      content: "The design is the best. I want to buy it."
+    },
+    {
+      evaluate_id: 5,
+      customer_id: 1,
+      content: "I've always wanted that bag."
+    },
+    {
+      evaluate_id: 5,
+      customer_id: 5,
+      content: "It will be very convenient for me when I go out."
+    },
+    {
+      evaluate_id: 7,
+      customer_id: 5,
+      content: "It's amazing. cool."
+    },
+    {
+      evaluate_id: 7,
+      customer_id: 3,
+      content: "This is good. Does it look good on me ~"
+    }
+  ]
+)
