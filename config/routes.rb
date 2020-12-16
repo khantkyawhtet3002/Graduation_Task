e3ed03069c41437142e23187cc7dd8bf6197d083
nodmentor2ygn <nodmentor2ygn@gmail.com>
2020-12-16 15:20:13 +0630

    resourse & root

--- config/routes.rb
+++ config/routes.rb
@@ -21,17 +21,4 @@ Rails.application.routes.draw do
   resources :customers
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
-# 例
-#   devise_for :users, :controllers => {
-#     :registrations => 'users/registrations',
-#     :sessions => 'users/sessions'
-#   }
-
-#   devise_scope :user do
-#     get "sign_in", :to => "users/sessions#new"
-#     get "sign_out", :to => "users/sessions#destroy"
-#   end
