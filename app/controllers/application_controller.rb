27bb4fcab488f6862fceb6cdf222519c19d8813c
nodmentor2ygn <nodmentor2ygn@gmail.com>
2020-12-16 15:26:12 +0630

    creating_app_contro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # ログイン後のリダイレクト先
   def after_sign_in_path_for(resource_or_scope)
     if maker_signed_in?
       maker_path(id: current_maker.id)
@@ -18,8 +17,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # # ログアウト後のリダイレクト先
-  # def after_sign_out_path_for(resource_or_scope)
-  #     products_path
-  # end
 end
